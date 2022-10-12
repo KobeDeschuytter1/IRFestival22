56ecf57d3fbcb42d6bcff123b641d210138affaf
kobedeschuytter1 <kobe.deschuytter@hotmail.com>
2022-10-12 16:37:16 +0200

    z

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  festivalName: "IRFestival",
-  apiBaseUrl: "https://????.azurewebsites.net/api/"
+  festivalName: 'IRFestival',
+  apiBaseUrl: 'https://irfestivalapikds.azurewebsites.net/api/settings',
 };
