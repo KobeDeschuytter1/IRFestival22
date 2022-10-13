b5eb3909420468657a3778050d65e82c0eecba92
kobedeschuytter1 <kobe.deschuytter@hotmail.com>
2022-10-13 12:05:32 +0200

    last execricie

---
+++ Backend/IRFestival.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,34 @@
+﻿using IRFestival.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
+namespace IRFestival.Api.Controllers
+{
+    public class ArticlesController : Controller
+    {
+
+        private CosmosClient _cosmosClient { get; set; }
+        private Container _websiteArticlesContainer { get; set; }
+
+        public ArticlesController(IConfiguration configuration)
+        {
+            _cosmosClient = new CosmosClient(configuration.GetConnectionString("CosmosConnection"));
+            _websiteArticlesContainer = _cosmosClient.GetContainer("IRFestivalArticles", "WebsiteArticles");
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateItemAsync()
+        {
+            var dummyArticle = new Article()
+            {
+                Date = DateTime.Now,
+                Message = "Test Message",
+                Status = "Status",
+                Tag = "Tag",
+                Title = "Title",
+                ImagePath = "ImagePath"
+            };
+            await _websiteArticlesContainer.CreateItemAsync(dummyArticle);
+            return Ok(dummyArticle);
+        }
+    }
+}
--- Backend/IRFestival.Api/Domain/Article.cs
+++ Backend/IRFestival.Api/Domain/Article.cs
@@ -1,21 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 
 namespace IRFestival.Api.Domain
 {
     public class Article
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
-
+        [JsonProperty(PropertyName = "Tag)]
         public string Tag { get; set; }
-
+        [JsonProperty(PropertyName = "Title")]
         public string Title { get; set; }
-
+        [JsonProperty(PropertyName = "Message")]
         public string Message { get; set; }
-
+        [JsonProperty(PropertyName = "ImagePath")]
         public string ImagePath { get; set; }
-
+        [JsonProperty(PropertyName = "Status")]
         public string Status { get; set; }
-
+        [JsonProperty(PropertyName = "Date")]
         public DateTime Date { get; set; }
     }
 
